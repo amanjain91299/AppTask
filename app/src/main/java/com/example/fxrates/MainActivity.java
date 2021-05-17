9f1eacdd2f4a58c7514f6fd9f9e3fcbbaff151b3
Pawan <meanstack4@gmail.com>
2021-05-17 20:15:33 +0530

    changes in code

--- app/src/main/java/com/example/fxrates/MainActivity.java
+++ app/src/main/java/com/example/fxrates/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.content.DialogInterface;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -15,13 +17,10 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.fxrates.adapter.RateAdapter;
-import com.example.fxrates.dao.FxRatesDAO;
 import com.example.fxrates.databinding.ActivityMainBinding;
 import com.example.fxrates.modal.Data;
-import com.example.fxrates.modal.FxRates;
 import com.example.fxrates.modal.Output;
 import com.example.fxrates.modal.Rates;
-import com.google.gson.JsonObject;
 
 import org.json.JSONObject;
 
@@ -46,13 +45,135 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
         al = new ArrayList<>();
+        adapter = new RateAdapter(this,al);
         getData();
+        binding.btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fromValue = binding.etConvert.getText().toString();
+                for(Output o : al){
+                    if(o.getKey().equals("GBP")){
+
+                    }
+                    else if(o.getKey().equals("HKD")){
+
+                    }
+                    else if(o.getKey().equals("IDR")){
+
+                    }
+                    else if(o.getKey().equals("ILS")){
+
+                    }
+                    else if(o.getKey().equals("DKK")){
+
+                    }
+                    else if(o.getKey().equals("INR")){
+
+                    }
+                    else if(o.getKey().equals("CHF")){
+
+                    }
+                    else if(o.getKey().equals("MXN")){
+
+                    }
+                    else if(o.getKey().equals("CZK")){
+
+                    }
+                    else if(o.getKey().equals("SGD")){
+
+                    }
+                    else if(o.getKey().equals("THB")){
+
+                    }
+                    else if(o.getKey().equals("HRK")){
+
+                    }
+                    else if(o.getKey().equals("MYR")){
+
+                    }
+                    else if(o.getKey().equals("NOK")){
+
+                    }
+                    else if(o.getKey().equals("CNY")){
+
+                    }
+                    else if(o.getKey().equals("BGN")){
+
+                    }
+                    else if(o.getKey().equals("PHP")){
+
+                    }
+                    else if(o.getKey().equals("SEK")){
+
+                    }
+                    else if(o.getKey().equals("PLN")){
+
+                    }
+                    else if(o.getKey().equals("ZAR")){
+
+                    }
+                    else if(o.getKey().equals("CAD")){
+
+                    }
+                    else if(o.getKey().equals("ISK")){
+
+                    }
+                    else if(o.getKey().equals("BRL")){
+
+                    }
+                    else if(o.getKey().equals("RON")){
+
+                    }
+                    else if(o.getKey().equals("NZD")){
+
+                    }
+                    else if(o.getKey().equals("TRY")){
+
+                    }
+                    else if(o.getKey().equals("JPY")){
+
+                    }
+                    else if(o.getKey().equals("RUB")){
+
+                    }
+                    else if(o.getKey().equals("KRW")){
+
+                    }
+                    else if(o.getKey().equals("USD")){
+
+                    }
+                    else if(o.getKey().equals("HUF")){
+
+                    }
+                    else if(o.getKey().equals("AUD")){
+
+                    }
+                }
+            }
+        });
         binding.btnRefersh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FxRates fxRates = FxRatesDAO.getRates(MainActivity.this);
-                showUpdatedData(fxRates);
-                Log.e("Rates:==>",fxRates.getRates());
+                DatabaseHelper helper = new DatabaseHelper(MainActivity.this);
+                SQLiteDatabase database = helper.getReadableDatabase();
+                Cursor c = database.rawQuery("select * from fxrate",null);
+                int i=2;
+                al.clear();
+                adapter.notifyDataSetChanged();
+                while(c.moveToNext()){
+                    while(i<=c.getColumnCount()-2){
+
+                        String key = c.getColumnName(i);
+                        String value = c.getString(i);
+                        if(!value.equals("")) {
+                            Output output = new Output(key, value);
+                            al.add(output);
+                        }
+                        i++;
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                database.close();
             }
         });
 
@@ -93,29 +214,32 @@ public class MainActivity extends AppCompatActivity {
 
     private void populateDataOnUi(Data data){
         Rates rates = data.getRates();
-        Log.e("Rates","==>"+rates.toString());
-        FxRates fxRates = new FxRates(data.getBase(),data.getDate(),rates.toString());
-        FxRatesDAO.saveRates(fxRates,MainActivity.this);
         try{
             al.clear();
+            adapter.notifyDataSetChanged();
             JSONObject obj = new JSONObject(rates.toString());
             Iterator<String> itr = obj.keys();
+
             while(itr.hasNext()){
                 String key = itr.next();
                 String value = obj.get(key).toString();
-                al.add(new Output(key,value));
+                al.add(new Output(key.toUpperCase(),value));
             }
+            saveDataIntoSqlite(al,data);
             adapter = new RateAdapter(MainActivity.this,al);
             binding.rv.setAdapter(adapter);
             binding.rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
             adapter.setOnClickListener(new RateAdapter.OnRecyclerViewClick() {
                 @Override
                 public void onItemClick(Output output, int positioin) {
-                  FxRatesDAO.deleteRates(output.getKey(),data,MainActivity.this,fxRates.getRates());
-                      al.remove(positioin);
-                      adapter.notifyDataSetChanged();
-                      Toast.makeText(MainActivity.this, "Data removed..", Toast.LENGTH_SHORT).show();
-
+                     DatabaseHelper helper = new DatabaseHelper(MainActivity.this);
+                     SQLiteDatabase database = helper.getWritableDatabase();
+                     ContentValues values = new ContentValues();
+                     values.put(output.getKey().toUpperCase(),"");
+                     database.update("fxrate",values,null,null);
+                     Toast.makeText(MainActivity.this, "Record Deleted", Toast.LENGTH_SHORT).show();
+                     al.remove(positioin);
+                     adapter.notifyDataSetChanged();
                 }
             });
         }
@@ -123,6 +247,21 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, ""+e, Toast.LENGTH_SHORT).show();
             Log.e("Error","====>"+e);
         }
+    }
+    private void saveDataIntoSqlite(ArrayList<Output> al,Data data){
+
+        ContentValues values = new ContentValues();
+        for(Output output : al){
+            values.put(output.getKey(),output.getValue());
+        }
+        values.put("base",data.getBase());
+        values.put("date",data.getDate());
+        DatabaseHelper helper  = new DatabaseHelper(MainActivity.this);
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.delete("fxrate",null,null);
+        db.insert("fxrate",null,values);
+        db.close();
     }
     public boolean isInternetConnected(){
         boolean networkStatus = false;
@@ -135,34 +274,5 @@ public class MainActivity extends AppCompatActivity {
         else
             networkStatus = false;
         return networkStatus;
-    }
-    private void showUpdatedData(FxRates fxRates){
-        try{
-            JSONObject obj = new JSONObject(fxRates.getRates());
-            Iterator<String> itr = obj.keys();
-            al.clear();
-            while(itr.hasNext()){
-                String key = itr.next();
-                String value = obj.get(key).toString();
-                al.add(new Output(key,value));
-            }
-            adapter = new RateAdapter(MainActivity.this,al);
-            binding.rv.setAdapter(adapter);
-            binding.rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-            adapter.setOnClickListener(new RateAdapter.OnRecyclerViewClick() {
-                @Override
-                public void onItemClick(Output output, int positioin) {
-                        FxRatesDAO.deleteRates(output.getKey(),data,MainActivity.this,fxRates.getRates());
-                        al.remove(positioin);
-                        adapter.notifyDataSetChanged();
-                        Toast.makeText(MainActivity.this, "Data removed..", Toast.LENGTH_SHORT).show();
-
-                }
-            });
-        }
-        catch (Exception e){
-            Toast.makeText(this, ""+e, Toast.LENGTH_SHORT).show();
-            Log.e("Error:::","====>"+e);
-        }
     }
 }
